15beb17873d9b94d12cf4e8790e71fe3f5925308
jeo <abdeldjalildh990@gmail.com>
2024-10-07 20:16:20 +0200

    half way event page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Welcome from "./pages/Welcome";
+import Events from "./pages/Events";
 import Root from "./layouts/Root";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Root />}>
           <Route path="" element={<Welcome />} />
+          <Route path="/events" element={<Events />} />
         </Route>
       </Routes>
     </Router>
