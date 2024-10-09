1bc29263914e53613890da3e623ca0f27ead435f
Salah Eddine Kadik <123268700+kadik23@users.noreply.github.com>
2024-10-09 23:24:48 +0100

    contact us feature (#1)
    * feat: implement contact us page
    
    * chore add .env example

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Welcome from "./pages/Welcome";
 import Root from "./layouts/Root";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Root />}>
           <Route path="" element={<Welcome />} />
+          <Route path="/contact-us" element={<Contact />} />
         </Route>
       </Routes>
     </Router>
