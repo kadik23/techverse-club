25b5f5e0491efebb5212b5e8e3ea409ea5e91ef0
unknown <kadiksalah03@gmail.com>
2024-10-13 16:37:59 +0100

    replace href with navlink

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import PersonCard from "../components/PersonCard";
 import { Button } from "../components/ui/button";
 import { Icon } from "@iconify-icon/react/dist/iconify.js";
@@ -14,6 +15,11 @@ function Welcome() {
         Hello techverse developers!
         <Icon icon="mdi:human-hello-variant" width="24" height="24" />
       </Button>
+      <NavLink to={'/event-overview/bootcamp'}>
+        <Button id="Home" className="font-geistMono bg-secondary">
+          Go to bootcamp event
+        </Button>
+      </NavLink>
       <PersonCard
         name="kadik salah eddine"
         departement={{
